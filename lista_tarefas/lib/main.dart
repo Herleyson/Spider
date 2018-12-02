21c83a6b93a58e64d14f6880f49936332e1e3adb
Herleyson Alves <herleyson100@hotmail.com>
2018-12-02 15:34:05 -0300

    Adicionado uma tela de introdução

--- lista_tarefas/lib/main.dart
+++ lista_tarefas/lib/main.dart
@@ -8,12 +8,112 @@ import 'package:path_provider/path_provider.dart';
 import 'package:intl/intl.dart';
 import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 
+import 'package:intro_views_flutter/Models/page_view_model.dart';
+import 'package:intro_views_flutter/intro_views_flutter.dart';
 
-void main(){
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+
+void main() {
+
+
   runApp(MaterialApp(
-    home: Home(),
+    home: Splash(),
   ));
 }
+
+
+
+class App extends StatelessWidget {
+  //making list of pages needed to pass in IntroViewsFlutter constructor.
+  final pages = [
+    PageViewModel(
+      pageColor: Colors.redAccent,
+      iconImageAssetPath: "Images/agenda.png",
+      body: Text(
+        'Com grandes poderes vêm grandes responsabilidades',
+      ),
+      title: Text('Spiderman'),
+      mainImage: Image.asset(
+        'Images/agenda.png',
+        height: 285.0,
+        width: 285.0,
+        alignment: Alignment.center,
+      ),
+      textStyle: TextStyle(fontFamily: 'MyFont', color: Colors.white),
+    ),
+    PageViewModel(
+      pageColor: Colors.black54,
+      iconImageAssetPath: "Images/agenda.png",
+      body: Text(
+        'Em homegagem ao grande heroi',
+      ),
+      title: Text('Stan Lee'),
+      mainImage: Image.asset(
+        'Images/stan.jpg',
+        height: 800.0,
+        width: 800.0,
+        alignment: Alignment.center,
+      ),
+      textStyle: TextStyle(fontFamily: 'MyFont', color: Colors.white),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+
+
+      return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tela de Introdução', //title of app
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ), //ThemeData
+      home: Builder(
+        builder: (context) => IntroViewsFlutter(
+          pages,
+          onTapDoneButton: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Home(),
+              ), //MaterialPageRoute
+            );
+          },
+          showSkipButton:
+          true, //Whether you want to show the skip button or not.
+          pageButtonTextStyles: TextStyle(
+            color: Colors.white,
+            fontSize: 18.0,
+          ),
+        ), //IntroViewsFlutter
+      ), //Builder
+    ); //Material App
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class Home extends StatefulWidget {
   @override
@@ -32,7 +132,7 @@ class _HomeState extends State<Home> {
 
   List _toDoList = [];
 
-  List _tarefas =  ["Salvar o mundo", "Estudar", "Trabalhar", "Piadas",];
+  List _tarefas = ["Salvar o mundo", "Estudar", "Trabalhar", "Piadas",];
 
   List<DropdownMenuItem<String>> _dropDownMenuItems;
   String _currentTarefa;
@@ -89,19 +189,22 @@ class _HomeState extends State<Home> {
 
         _saveData();
       });
-    }catch(e){
+    } catch (e) {
 
     }
   }
 
-  Future<Null> _refresh() async{
+  Future<Null> _refresh() async {
     await Future.delayed(Duration(seconds: 1));
 
     setState(() {
-      _toDoList.sort((a, b){
-        if(a["ok"] && !b["ok"]) return 1;
-        else if(!a["ok"] && b["ok"]) return -1;
-        else return 0;
+      _toDoList.sort((a, b) {
+        if (a["ok"] && !b["ok"])
+          return 1;
+        else if (!a["ok"] && b["ok"])
+          return -1;
+        else
+          return 0;
       });
 
       _saveData();
@@ -113,14 +216,14 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Lista do Spider"),
-        backgroundColor: Colors.red,
-        centerTitle: true,
+        appBar: AppBar(
+          title: Text("Lista do Spider"),
+          backgroundColor: Colors.red,
+          centerTitle: true,
 
 
-      ),
-      body:Container(
+        ),
+        body: Container(
           decoration: new BoxDecoration(
             image: new DecorationImage(
               image: new AssetImage("Images/spider5.jpeg"),
@@ -130,94 +233,97 @@ class _HomeState extends State<Home> {
           child:
 
 
-      Column(
-        children: <Widget>[
+          Column(
+            children: <Widget>[
 
-          Container(
-            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    controller: _toDoController,
-                    decoration: InputDecoration(
-                        labelText: "Nova Tarefa",
-                        labelStyle: TextStyle(color: Colors.white)
+              Container(
+                padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: TextField(
+                          controller: _toDoController,
+                          decoration: InputDecoration(
+                              labelText: "Nova Tarefa",
+                              labelStyle: TextStyle(color: Colors.white)
+                          ),
+                          style: TextStyle(color: Colors.white),
+                        )
                     ),
-                    style: TextStyle(color: Colors.white),
-                  )
+                    RaisedButton(
+                      color: Colors.red,
+                      child: Text("ADD"),
+                      textColor: Colors.blue,
+                      onPressed: _addToDo,
+                    )
+                  ],
                 ),
-                RaisedButton(
-                  color: Colors.red,
-                  child: Text("ADD"),
-                  textColor: Colors.blue,
-                  onPressed: _addToDo,
-                )
-              ],
-            ),
-          ),Container(
-            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+              ), Container(
+                padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
 
-           child: Row(
-             children:<Widget>[
-               Expanded(
-             child: DateTimePickerFormField(
-               controller: _dateController,
-               format: dateFormat,
+                child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: DateTimePickerFormField(
+                          controller: _dateController,
+                          format: dateFormat,
 
-               decoration: InputDecoration(
-                   labelText: 'Data',
-                   labelStyle: TextStyle(color: Colors.white)
+                          decoration: InputDecoration(
+                              labelText: 'Data',
+                              labelStyle: TextStyle(color: Colors.white)
 
-               ),
-               style:TextStyle(color: Colors.white) ,
+                          ),
+                          style: TextStyle(color: Colors.white),
 
-               onChanged: (dt) => setState(() => date = dt),
-             ),
-             ),
-          Container(
-          padding: EdgeInsets.fromLTRB(10.0, 27.0, 7.0, 1.0),
-          child:
-          Theme(
-            data: Theme.of(context).copyWith(
-              canvasColor: Colors.blue,
-            ),
-            child:DropdownButton(
-            value: _currentTarefa,
-             items: _dropDownMenuItems,
+                          onChanged: (dt) => setState(() => date = dt),
+                        ),
+                      ),
+                      Container(
+                          padding: EdgeInsets.fromLTRB(10.0, 27.0, 7.0, 1.0),
+                          child:
+                          Theme(
+                            data: Theme.of(context).copyWith(
+                              canvasColor: Colors.blue,
+                            ),
+                            child: DropdownButton(
+                              value: _currentTarefa,
+                              items: _dropDownMenuItems,
 
 
-              style:TextStyle(color: Colors.white) ,
+                              style: TextStyle(color: Colors.white),
 
-              onChanged: changedDropDownItem,
+                              onChanged: changedDropDownItem,
 
-          ),
-          )
-          )
-             ]
-           ),
-
-          ),
-
-
-          Expanded(
-            child: RefreshIndicator(onRefresh: _refresh,
-              child: ListView.builder(
-                  padding: EdgeInsets.only(top: 10.0),
-                  itemCount: _toDoList.length,
-                  itemBuilder: buildItem
+                            ),
+                          )
+                      )
+                    ]
+                ),
 
               ),
-            ),
-          )
-        ],
-      ),
-      ) );
+
+
+              Expanded(
+                child: RefreshIndicator(onRefresh: _refresh,
+                  child: ListView.builder(
+                      padding: EdgeInsets.only(top: 10.0),
+                      itemCount: _toDoList.length,
+                      itemBuilder: buildItem
+
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 
-  Widget buildItem(BuildContext context, int index){
+  Widget buildItem(BuildContext context, int index) {
     return Dismissible(
-      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      key: Key(DateTime
+          .now()
+          .millisecondsSinceEpoch
+          .toString()),
       background: Container(
         color: Colors.red,
         child: Align(
@@ -227,21 +333,22 @@ class _HomeState extends State<Home> {
       ),
       direction: DismissDirection.startToEnd,
       child: CheckboxListTile(
-        title: Text(_toDoList[index]["title"],style: TextStyle (color: Colors.white),),
+        title: Text(
+          _toDoList[index]["title"], style: TextStyle(color: Colors.white),),
 
         value: _toDoList[index]["ok"],
 
         secondary: CircleAvatar(
           child: Icon(_toDoList[index]["ok"] ?
           Icons.check : Icons.error),),
-        onChanged: (c){
+        onChanged: (c) {
           setState(() {
             _toDoList[index]["ok"] = c;
             _saveData();
           });
         },
       ),
-      onDismissed: (direction){
+      onDismissed: (direction) {
         setState(() {
           _lastRemoved = Map.from(_toDoList[index]);
           _lastRemovedPos = index;
@@ -262,7 +369,6 @@ class _HomeState extends State<Home> {
           );
 
           Scaffold.of(context).showSnackBar(snack);
-
         });
       },
     );
@@ -292,3 +398,39 @@ class _HomeState extends State<Home> {
 
 }
 
+class Splash extends StatefulWidget {
+  @override
+  SplashState createState() => new SplashState();
+}
+
+class SplashState extends State<Splash> {
+  Future checkFirstSeen() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool _seen = (prefs.getBool('seen') ?? false);
+
+    if (_seen) {
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => new Home()));
+    } else {
+      prefs.setBool('seen', true);
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => new App()));
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+      checkFirstSeen();
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+    child: new Text('Carregando...'),
+      ),
+    );
+  }
+}
